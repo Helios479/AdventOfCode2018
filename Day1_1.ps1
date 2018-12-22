f6140610a3ea298f276218e0e950a1dc25968ea8
Dan Lee <del001@uark.edu>
2018-12-21 18:47:10 -0600

    Day 1

---
+++ Day1_1.ps1
@@ -0,0 +1,7 @@
+﻿$Input = Get-Content "$PSScriptRoot\Inputs\Day1_Input.txt"
+$Freq = 0
+ForEach ($Entry in $Input){
+    Write-Progress -Activity "Adding Frequency" -Status "$Freq$Entry"
+    $Freq += $Entry #Updates frequency with new input
+}
+Return $Freq
---
+++ Day1_2.ps1
@@ -0,0 +1,13 @@
+﻿$Input = Get-Content "$PSScriptRoot\Inputs\Day1_Input.txt"
+$Freq = 0
+$History = New-Object System.Collections.ArrayList
+While ($true){
+    ForEach ($Entry in $Input){
+        Write-Progress -Activity "Testing Frequency $($History.Count)" -Status "$Freq$Entry"
+        [void]$History.Add($Freq) #Add frequency to collection of all frequencies
+        $Freq += $Entry
+        If ($Freq -in $History){
+            Return $Freq #Return first correct answer
+        }
+    }
+}
