73d26476f7ded82541d18ec6eda7f0c9bc2fc624
Dan Lee <del001@uark.edu>
2018-12-23 20:54:40 -0600

    Day 4

---
+++ Day4_1.ps1
@@ -0,0 +1,57 @@
+$Input = Get-Content $PSScriptRoot\Inputs\Day4_Input.txt
+#Organize Entries
+$Events = New-Object System.Collections.ArrayList
+ForEach ($Entry in $Input){
+    $Regex = [regex]::Match($Entry,'\[(.*)\]\s(.*)')
+    $Message = $Regex.Captures.Groups.Value[2]
+    If ($Message -like 'Guard*'){
+        $MessageRegex = [regex]::Match($Message,'#([0-9]*)\s(.*)')
+        $Guard = $MessageRegex.Captures.Groups.Value[1]
+        $Message = $MessageRegex.Captures.Groups.Value[2]
+    }
+    Else {
+        $Guard = $null
+        $Message = $Message
+    }
+    $EntryObject = [PSCustomObject]@{
+        'Date' = [DateTime]$Regex.Captures.Groups.Value[1]
+        'Guard' = $Guard
+        'Action' = $Message
+    }
+    [void]$Events.Add($EntryObject)
+}
+#Sort and assign guards
+$Events = $Events | Sort Date
+ForEach ($Event in $Events){
+    If ($Event.Guard) {
+        $Guard = $Event.Guard
+    }
+    Else {
+        $Event.Guard = $Guard
+    }
+}
+#Find sleepiest guard
+$OptimumTime = 0
+$TotalMinutesAsleep = 0
+ForEach ($Guard in ($Events.Guard | Select -Unique)){
+    $MinutesAsleep = @()
+    #Get minutes asleep
+    $Shifts = $Events | Where-Object Guard -EQ $Guard
+    $ShiftIndex = 0..($Shifts.Count - 1)
+    ForEach ($Index in $ShiftIndex){
+        If ($Shifts[$Index].Action -eq 'falls asleep'){
+            $Start = $Shifts[$Index]
+            $End = $Shifts[$Index + 1]
+            For ($i = $Start.Date.Minute ; $i -lt $End.Date.Minute ; $i ++){
+                $MinutesAsleep += $i
+            }
+        }
+    }
+    If ($MinutesAsleep.Count -gt $TotalMinutesAsleep){
+        $TotalMinutesAsleep = $MinutesAsleep.Count
+        #Get sleepiest minute
+        $SleepiestMinute = ($MinutesAsleep | Group-Object | Sort Count -Descending)[0]
+        $OptimumTime = [int]$Guard * [int]$SleepiestMinute.Name
+    }
+}
+Return $OptimumTime
